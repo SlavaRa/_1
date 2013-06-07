f5f7ae46586e5d5fa0ea7c67cc1f6773c589cd77
slavara <ratdg@ya.ru>
2013-06-07 07:31:13 +0400

    -- first commit

---
+++ src/slavara/Main.hx
@@ -0,0 +1,43 @@
+package slavara;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.Lib;
+import haxe.Log;
+import slavara.haxe.core.utils.Validate;
+
+/**
+ * @author SlavaRa
+ */
+class Main extends Sprite {
+
+	public static function main() {
+		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		Lib.current.addChild(new Main());
+	}
+
+	public function new() {
+		super();	
+		addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+	
+	function onAddedToStage(_) {
+		removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+		stage.addEventListener(Event.RESIZE, onResize);
+		#if ios
+		haxe.Timer.delay(initialize, 100); // iOS 6
+		#else
+		initialize();
+		#end
+		
+	}
+	
+	function onResize(_) {
+		
+	}
+	
+	function initialize() {
+	}
+
+}
