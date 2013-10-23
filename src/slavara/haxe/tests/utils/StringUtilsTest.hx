5c80cc6bb67161bea4741ee7265a48c68510b752
slavara <ratdg@ya.ru>
2013-10-23 22:41:14 +0400

    -- added empty StringUtilsTest

---
+++ src/slavara/haxe/tests/utils/StringUtilsTest.hx
@@ -0,0 +1,21 @@
+package slavara.haxe.tests.utils;
+
+/**
+ * @author SlavaRa
+ */
+class StringUtilsTest {
+
+	public function new() {
+	}
+	
+	@Test
+	public function isNullOrEmptyTest() {
+		//TODO: implement me
+	}
+	
+	@Test
+	public function trimTest() {
+		//TODO implement me
+	}
+	
+}
