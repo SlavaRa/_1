708670d42def3306f2a65b08eb2f6f4885add1b4
slavara <ratdg@ya.ru>
2013-10-23 22:27:25 +0400

    -- added ValidateUtils

---
+++ src/slavara/haxe/core/utils/ValidateUtils.hx
@@ -0,0 +1,16 @@
+package slavara.haxe.core.utils;
+
+/**
+ * @author SlavaRa
+ */
+class ValidateUtils {
+	
+	public static function isNull(d:Dynamic):Bool {
+		return d == null;
+	}
+	
+	public static function isNotNull(d:Dynamic):Bool {
+		return d != null;
+	}
+	
+}
