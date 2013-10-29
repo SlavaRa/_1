d3764f31748115225469437d37e3c3cfd62a3434
slavara <ratdg@ya.ru>
2013-10-29 09:56:32 +0400

    -- add empty data test.

---
+++ src/test/slavara/haxe/core/models/DataContainerTest.hx
@@ -0,0 +1,14 @@
+package slavara.haxe.core.models;
+import slavara.haxe.core.models.Data;
+
+/**
+ * @author SlavaRa
+ */
+class DataContainerTest extends Data {
+
+	public function new() {
+		super();
+	}
+	
+	//TODO: implement me
+}
---
+++ src/test/slavara/haxe/core/models/DataTest.hx
@@ -0,0 +1,12 @@
+package slavara.haxe.core.models;
+
+/**
+ * @author SlavaRa
+ */
+class DataTest {
+
+	public function new() {
+	}
+	
+	//TODO: implement me
+}
