f06bb7042578361ea7cdacd8f4be4965ce3ccced
slavara <ratdg@ya.ru>
2014-01-31 21:17:16 +0400

    slavara.haxe.core.display.DisplayObject move to package slavara.haxe.core.TypeDefs

--- src/slavara/haxe/core/TypeDefs.hx
+++ src/slavara/haxe/core/TypeDefs.hx
@@ -1,6 +1,7 @@
-package slavara.haxe.core.display;
+package slavara.haxe.core;
 
 #if !(flash11 && starling)
+typedef DisplayObject = flash.display.DisplayObject;
 typedef BaseSprite = slavara.haxe.core.display.openfl.BaseSprite;
 typedef ResourceSprite = slavara.haxe.core.display.openfl.ResourceSprite;
 #else
