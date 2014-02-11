e994ec3b4e67b38ae72c10ffb55042e577d7bbf9
slavara <ratdg@ya.ru>
2014-02-11 16:39:25 +0400

    -- add slavara.haxe.game.Models.UnknownProto -- add slavara.haxe.game.Models.UnknownData

---
+++ src/slavara/haxe/game/Models.hx
@@ -0,0 +1,80 @@
+package slavara.haxe.game;
+import slavara.haxe.core.Errors.NullArgumentError;
+import slavara.haxe.core.events.models.DataBaseEvent;
+import slavara.haxe.core.Models.DataValueObjectContainer;
+import slavara.haxe.core.StateMachine;
+import slavara.haxe.game.Interfaces.IUnknown;
+using slavara.haxe.core.utils.Utils.ValidateUtil;
+using Reflect;
+using Std;
+
+class UnknownProto extends DataValueObjectContainer implements IUnknown {
+
+	public function new() {
+		super();
+		initialize();
+	}
+	
+	public var id(get, null):Int;
+	public var desc(get, null):String;
+	
+	@:noCompletion var _id:Int;
+	@:noCompletion var _desc:String;
+	
+	@:noCompletion inline function get_id():Int return _id;
+	@:noCompletion inline function get_desc():String return _desc;
+	
+	function initialize() { }
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.hasField("id")) {
+			_id = input.getProperty("id");
+		}
+		if(input.hasField("desc")) {
+			_desc = input.getProperty("desc");
+		}
+	}
+}
+
+/**
+ * @author SlavaRa
+ */
+class UnknownData extends DataValueObjectContainer implements IUnknown {
+
+	function new(proto:UnknownProto) {
+		#if debug
+		if(proto.isNull()) throw new NullArgumentError("proto");
+		#end
+		super();
+		_proto = proto;
+		initialize();
+	}
+	
+	public var stateMachine(default, null):StateMachine;
+	public var id(get, null):Int;
+	public var desc(get, null):String;
+	
+	var _id:Int;
+	var _desc:String;
+	var _proto:UnknownProto;
+	
+	@:noCompletion inline function get_id():Int return _id;
+	@:noCompletion inline function get_desc():String return _desc;
+	
+	function initialize() {
+		stateMachine = new StateMachine();
+	}
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.hasField("id")) {
+			_id = input.getProperty("id");
+		}
+		if(input.hasField("desc")) {
+			_desc = input.getProperty("desc");
+		}
+	}
+}
