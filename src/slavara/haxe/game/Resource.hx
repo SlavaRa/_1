54942e6aa1a0ceebc26d8f73cbee57a653d294df
slavara <ratdg@ya.ru>
2014-02-24 09:48:51 +0400

    -- add module Resource.

---
+++ src/slavara/haxe/game/Resource.hx
@@ -0,0 +1,38 @@
+package slavara.haxe.game;
+import slavara.haxe.game.Models.UnknownProto;
+import slavara.haxe.game.Resource.ResRef;
+using Reflect;
+
+/**
+ * @author SlavaRa
+ */
+class ResRef extends UnknownProto {
+	public function new() super();
+	
+	public var link(default, null):String;
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.hasField("link")) {
+			link = input.getProperty("link");
+		}
+	}
+}
+
+/**
+ * @author SlavaRa
+ */
+class SWFResRef extends ResRef {
+	public function new() super();
+	
+	public var swf(default, null):String;
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.hasField("swf")) {
+			swf = input.getProperty("swf");
+		}
+	}
+}
