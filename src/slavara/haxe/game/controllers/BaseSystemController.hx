ab108955abbce97441b34393c854459276264c4a
slavara <ratdg@ya.ru>
2014-11-15 23:57:15 +0300

    Add TODO

--- src/slavara/haxe/game/controllers/BaseSystemController.hx
+++ src/slavara/haxe/game/controllers/BaseSystemController.hx
@@ -11,4 +11,9 @@ class BaseSystemController extends BaseController {
 	public function new(container:DisplayObjectContainer) super(container, Type.createInstance(getUniverseDataType(), []));
 
 	function getUniverseDataType():Class<Dynamic> return BaseUniverseData;
+	//TODO slavara: get ServerType
+	//TODO slavara: get StatType
+	//TODO slavara: get UserType
+	//TODO slavara: get ScreenType
+	//TODO slavara: get SoundType
 }
